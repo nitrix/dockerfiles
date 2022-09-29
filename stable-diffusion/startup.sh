fbcfe68ac207c2c8495cd17f84667202089a5600
Alex Belanger <nitrix@nitrix.me>
2022-09-29 19:28:24 -0400

    Added stable diffusion.

---
+++ stable-diffusion/startup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This is mounted.
+cd /mnt/sd
+
+if [ ! -f "stable-diffusion-ui-linux.tar.xz" ]; then
+	echo "Downloading stable diffusion"
+    wget https://github.com/cmdr2/stable-diffusion-ui/releases/download/v2.16/stable-diffusion-ui-linux.tar.xz
+else
+	echo "Skipping download of stable diffusion"
+fi
+
+
+if [ ! -f "/mnt/sd/stable-diffusion-ui" ]; then
+	echo "Extracting stable diffusion"
+	tar xf stable-diffusion-ui-linux.tar.xz
+fi
+
+echo "Running stable diffusion"
+cd /mnt/sd/stable-diffusion-ui
+/mnt/sd/stable-diffusion-ui/start.sh
